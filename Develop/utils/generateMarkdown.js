93176d695dba74ed120dfaf05514be2f633daf36
Ian Danas <ianmdanas@gmail.com>
2023-04-19 09:33:21 -0700

    adding default return statment to license function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,6 +9,9 @@ function renderLicenseLink(license) {}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  if (license = ''){
+    return ''
+  } 
  return `This project is covered under the ${license} license`
 }
 
